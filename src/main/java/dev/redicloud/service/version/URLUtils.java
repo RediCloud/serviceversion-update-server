29ef32daa2a444e52d4a89117bbd3bf0faa8069c
Suqatri <paul@suqatri.net>
2022-08-11 18:24:21 +0200

    Create URLUtils.java

---
+++ src/main/java/dev/redicloud/service/version/URLUtils.java
@@ -0,0 +1,33 @@
+package dev.redicloud.service.version;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+
+public class URLUtils {
+
+    public static String getContent(String rwaUrl) throws Exception {
+        URL url = new URL(rwaUrl);
+        Scanner sc = new Scanner(url.openStream());
+        StringBuffer builder = new StringBuffer();
+        while(sc.hasNext()) {
+            builder.append(sc.next());
+        }
+        String result = builder.toString();
+        result = result.replaceAll("<[^>]*>", "");
+        return result;
+    }
+
+    public static Collection<String> getLines(String rwaUrl) throws Exception {
+        URL url = new URL(rwaUrl);
+        Scanner sc = new Scanner(url.openStream());
+        List<String> list = new ArrayList<>();
+        while(sc.hasNext()) {
+            list.add(sc.next());
+        }
+        return list;
+    }
+
+}
