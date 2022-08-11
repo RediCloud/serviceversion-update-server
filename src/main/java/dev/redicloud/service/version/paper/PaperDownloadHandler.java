eec5c3369bf09288a49f67f0de569dfdae497032
Suqatri <paul@suqatri.net>
2022-08-11 18:24:30 +0200

    Create PaperDownloadHandler.java

---
+++ src/main/java/dev/redicloud/service/version/paper/PaperDownloadHandler.java
@@ -0,0 +1,32 @@
+package dev.redicloud.service.version.paper;
+
+import dev.redicloud.service.version.ServiceVersion;
+import dev.redicloud.service.version.URLUtils;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+public class PaperDownloadHandler implements Handler {
+
+    @Override
+    public void handle(@NotNull Context ctx) throws Exception {
+        ServiceVersion version = ServiceVersion.fromApiName(ctx.pathParam("version"));
+        String build = ctx.pathParam("build");
+        if(build.equalsIgnoreCase("latest") || build.equalsIgnoreCase("newest")){
+            String content = URLUtils.getContent("https://papermc.io/api/v2/projects/paper/versions/" + version.getApiName() + "/builds");
+            JSONObject json = new JSONObject(content);
+            int i = 0;
+            for (int i1 = 0; i1 < json.getJSONArray("builds").length(); i1++) {
+                JSONObject entry = json.getJSONArray("builds").getJSONObject(i1);
+                if(entry.getInt("build") > i){
+                    i = entry.getInt("build");
+                }
+            }
+            build = String.valueOf(i);
+        }
+        ctx.redirect("https://api.papermc.io/v2/projects/paper/versions/" + version.getApiName()
+                + "/builds/" + build
+                + "/downloads/paper-" + version.getApiName() + "-" + build +".jar");
+    }
+}
